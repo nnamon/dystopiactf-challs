13f685353599614e884239c1343be19b42de269b
glit3h <glit3h@iwanttoplayaga.me>
2015-06-04 19:22:04 +0800

    removed trailing whitespaces

--- challenges/crypto/dividedwestand/static/dividedwestand.py
+++ challenges/crypto/dividedwestand/static/dividedwestand.py
@@ -66,26 +66,26 @@ def gen_primes():
     # indefinitely, but only as long as required by the current
     # number being tested.
     #
-    D = {}  
+    D = {}
 
     # The running integer that's checked for primeness
-    q = 2  
+    q = 2
 
     while True:
         if q not in D:
             # q is a new prime.
             # Yield it and mark its first multiple that isn't
             # already marked in previous iterations
-            # 
-            yield q        
+            #
+            yield q
             D[q * q] = [q]
         else:
             # q is composite. D[q] is the list of primes that
             # divide it. Since we've reached q, we no longer
-            # need it in the map, but we'll mark the next 
+            # need it in the map, but we'll mark the next
             # multiples of its witnesses to prepare for larger
             # numbers
-            # 
+            #
             for p in D[q]:
                 D.setdefault(p + q, []).append(p)
             del D[q]
