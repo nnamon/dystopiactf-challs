1ce4cf3d0fd6f002d40a58d1ba8cee41f6ba4efb
Ngo Wei Lin <Creastery@gmail.com>
2015-06-16 21:36:18 +0800

    Small change to fix quotes

--- challenges/forensics/raidzero/grader/grader.py
+++ challenges/forensics/raidzero/grader/grader.py
@@ -1,5 +1,4 @@
 def grade(arg, key):
-    if "flag{n1c3_R41d_m8_GooD_J0b}”.lower() == key.lower():
-        return True, “Darn, you defeated my encrypted RAID0 scheme.”
+    if "flag{n1c3_R41d_m8_GooD_J0b}".lower() == key.lower():
+        return True, "Darn, you defeated my encrypted RAID0 scheme."
     else:
-        return False, “You failed the raid. Try harder.“
