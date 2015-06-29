3c711f1684803aacb6db9559d627852dab64c0b9
glit3h <glit3h@iwanttoplayaga.me>
2015-06-29 22:04:07 +0800

    Update lolttery.c to match server version

--- challenges/programming/lolttery/grader/lolttery.c
+++ challenges/programming/lolttery/grader/lolttery.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <time.h>
+#include <unistd.h>
 
 
 void print_banner()
@@ -11,12 +13,20 @@ void print_banner()
     puts(" \\ \\_____\\ \\_____\\ \\_____\\ \\ \\_\\   \\ \\_\\ \\ \\_____\\ \\_\\ \\_\\/\\_____\\ ");
     puts("  \\/_____/\\/_____/\\/_____/  \\/_/    \\/_/  \\/_____/\\/_/ /_/\\/_____/ ");
 }
+
+void handler(int sig)
+{
+    puts("\nYOU'RE SO SLOW THAT WE GAVE YOUR TICKETS AWAY!");
+
+    exit(0);
+}
+
 int main()
 {
     srand(time(NULL));
 
     int secret = rand() % 1000000 + 1;
-    char * guess;
+    char guess[7];
 
     print_banner();
 
@@ -25,18 +35,24 @@ int main()
     puts("**************************************************************************************");
     fflush(stdout);
 
+    signal(SIGALRM, handler);
+
     for(int i = 0; i < 20; i++) {
         printf("TICKET #%02d => ", i+1);
         fflush(stdout);
-        scanf("%s", guess);
+        alarm(2);
+        fgets(guess, sizeof(guess), stdin);
 
         if (atoi(guess) == secret) {
             puts("flag{Y0u_4r3_RNG_G0d}");
+            fflush(stdout);
             exit(0);
         } else if(atoi(guess) > secret) {
             puts("THE MYSTICAL COW SAYS: TOO HIGH");
+            fflush(stdout);
         } else {
             puts("THE MYSTICAL COW SAYS: TOO LOW");
+            fflush(stdout);
         }
     }
     puts("\nYOU RAN OUT OF TICKETS. SPENT ALL YOUR MONEY. YOU ARE BROKE. HAVE A NICE LIFE!");
