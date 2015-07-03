189cebb65227d5d56dbec721c568762d722d8b1d
glit3h <glit3h@iwanttoplayaga.me>
2015-07-03 19:40:48 +0800

    fixed quotes

--- challenges/programming/cropcircles/grader/grader.py
+++ challenges/programming/cropcircles/grader/grader.py
@@ -1,5 +1,5 @@
 def grade(arg, key):
-    if "flag{D1dy0uenJ0yunScr4mBLINGth15}”.lower() == key.lower():
-        return True, “Yup that seems correct. These aliens such trolls eh?”
+    if "flag{D1dy0uenJ0yunScr4mBLINGth15}".lower() == key.lower():
+        return True, "Yup that seems correct. These aliens such trolls eh?"
     else:
-        return False, “Seems like humans cannot converse with extraterrestrial beings yet.”
+        return False, "Seems like humans cannot converse with extraterrestrial beings yet."
