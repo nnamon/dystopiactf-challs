ba246333a31b4879b18677c9020f2256b2219fcd
amon <jergorn93@gmail.com>
2015-07-03 11:15:09 +0800

    New challenge: datingservice

---
+++ challenges/crypto/francesha/grader/solve.py
@@ -0,0 +1,43 @@
+data=r"""
+x_{10} \oplus x_{9} \oplus x_{7} \oplus x_{12} \oplus x_{5} \oplus x_{8} \oplus x_{3} \oplus x_{6} \oplus x_{11} \oplus x_{1} \oplus x_{13} = 108\\
+x_{3} \oplus x_{11} \oplus x_{6} \oplus x_{2} \oplus x_{1} \oplus x_{8} \oplus x_{0} \oplus x_{10} \oplus x_{7} \oplus x_{4} \oplus x_{9} = 122\\
+x_{1} \oplus x_{12} \oplus x_{6} \oplus x_{11} \oplus x_{8} \oplus x_{7} \oplus x_{9} \oplus x_{3} \oplus x_{13} \oplus x_{4} \oplus x_{0} = 96\\
+x_{12} \oplus x_{3} \oplus x_{13} \oplus x_{7} \oplus x_{2} \oplus x_{6} \oplus x_{10} \oplus x_{4} \oplus x_{8} \oplus x_{11} \oplus x_{0} = 115\\
+x_{5} \oplus x_{12} \oplus x_{4} \oplus x_{8} \oplus x_{6} \oplus x_{9} \oplus x_{0} \oplus x_{7} \oplus x_{1} \oplus x_{10} \oplus x_{11} = 115\\
+x_{11} \oplus x_{5} \oplus x_{7} \oplus x_{2} \oplus x_{10} \oplus x_{12} \oplus x_{6} \oplus x_{3} \oplus x_{0} \oplus x_{13} \oplus x_{1} = 121\\
+x_{4} \oplus x_{0} \oplus x_{10} \oplus x_{5} \oplus x_{11} \oplus x_{7} \oplus x_{8} \oplus x_{13} \oplus x_{1} \oplus x_{3} \oplus x_{2} = 112\\
+x_{9} \oplus x_{4} \oplus x_{10} \oplus x_{13} \oplus x_{0} \oplus x_{11} \oplus x_{6} \oplus x_{5} \oplus x_{3} \oplus x_{7} \oplus x_{12} = 126\\
+x_{13} \oplus x_{10} \oplus x_{4} \oplus x_{0} \oplus x_{3} \oplus x_{2} \oplus x_{12} \oplus x_{1} \oplus x_{11} \oplus x_{6} \oplus x_{7} = 98\\
+x_{13} \oplus x_{11} \oplus x_{10} \oplus x_{12} \oplus x_{7} \oplus x_{4} \oplus x_{9} \oplus x_{2} \oplus x_{1} \oplus x_{6} \oplus x_{5} = 102\\
+x_{2} \oplus x_{0} \oplus x_{5} \oplus x_{11} \oplus x_{6} \oplus x_{9} \oplus x_{4} \oplus x_{1} \oplus x_{12} \oplus x_{13} \oplus x_{10} = 113\\
+x_{4} \oplus x_{11} \oplus x_{3} \oplus x_{9} \oplus x_{5} \oplus x_{13} \oplus x_{10} \oplus x_{8} \oplus x_{7} \oplus x_{2} \oplus x_{1} = 99\\
+x_{6} \oplus x_{2} \oplus x_{7} \oplus x_{8} \oplus x_{1} \oplus x_{0} \oplus x_{9} \oplus x_{5} \oplus x_{3} \oplus x_{10} \oplus x_{4} = 118\\
+x_{4} \oplus x_{8} \oplus x_{2} \oplus x_{10} \oplus x_{7} \oplus x_{12} \oplus x_{1} \oplus x_{0} \oplus x_{5} \oplus x_{6} \oplus x_{11} = 100\\
+x_{10} \oplus x_{13} \oplus x_{5} \oplus x_{6} \oplus x_{7} \oplus x_{4} \oplus x_{11} \oplus x_{0} \oplus x_{2} \oplus x_{9} \oplus x_{8} = 109\\
+x_{3} \oplus x_{4} \oplus x_{7} \oplus x_{6} \oplus x_{0} \oplus x_{11} \oplus x_{5} \oplus x_{9} \oplus x_{8} \oplus x_{2} \oplus x_{13} = 117\\
+x_{1} \oplus x_{5} \oplus x_{4} \oplus x_{3} \oplus x_{2} \oplus x_{10} \oplus x_{13} \oplus x_{8} \oplus x_{6} \oplus x_{7} \oplus x_{12} = 108\\
+x_{0} \oplus x_{7} \oplus x_{8} \oplus x_{12} \oplus x_{9} \oplus x_{10} \oplus x_{3} \oplus x_{1} \oplus x_{11} \oplus x_{2} \oplus x_{6} = 99\\
+x_{5} \oplus x_{8} \oplus x_{6} \oplus x_{12} \oplus x_{9} \oplus x_{0} \oplus x_{3} \oplus x_{10} \oplus x_{4} \oplus x_{1} \oplus x_{13} = 108\\
+x_{6} \oplus x_{2} \oplus x_{13} \oplus x_{1} \oplus x_{3} \oplus x_{5} \oplus x_{8} \oplus x_{0} \oplus x_{4} \oplus x_{10} \oplus x_{12} = 123\\
+x_{0} \oplus x_{4} \oplus x_{11} \oplus x_{5} \oplus x_{3} \oplus x_{12} \oplus x_{9} \oplus x_{6} \oplus x_{1} \oplus x_{8} \oplus x_{7} = 107\\
+x_{6} \oplus x_{8} \oplus x_{0} \oplus x_{1} \oplus x_{13} \oplus x_{9} \oplus x_{2} \oplus x_{12} \oplus x_{4} \oplus x_{10} \oplus x_{11} = 107\\
+x_{6} \oplus x_{2} \oplus x_{12} \oplus x_{3} \oplus x_{11} \oplus x_{13} \oplus x_{5} \oplus x_{8} \oplus x_{4} \oplus x_{0} \oplus x_{9} = 120\\
+x_{6} \oplus x_{10} \oplus x_{11} \oplus x_{5} \oplus x_{0} \oplus x_{2} \oplus x_{1} \oplus x_{7} \oplus x_{3} \oplus x_{9} \oplus x_{8} = 97\\
+x_{11} \oplus x_{9} \oplus x_{2} \oplus x_{1} \oplus x_{10} \oplus x_{5} \oplus x_{8} \oplus x_{12} \oplus x_{6} \oplus x_{7} \oplus x_{3} = 123\\
+x_{9} \oplus x_{0} \oplus x_{11} \oplus x_{13} \oplus x_{7} \oplus x_{2} \oplus x_{3} \oplus x_{4} \oplus x_{5} \oplus x_{1} \oplus x_{6} = 100\\
+"""
+
+lines = data.strip().split("\n")
+eqns = []
+s = set()
+for i in lines:
+    eqn, res = i.split(" = ")
+    res = int(res[:-2])
+    eqn = [int(i[3:-1]) for i in eqn.split(" \\oplus ")]
+    eqns.append([eqn, res])
+        
+for i in range(14):
+    print "(declare-fun x%d () (_ BitVec 8))" % i
+
+for i in eqns:
+    print "(assert (= (bvxor %s) #x%02x))" % (" ".join("x%d" % j for j in i[0]), i[1])
---
+++ challenges/web/datingservice/grader/grader.py
@@ -0,0 +1,5 @@
+def grade(arg, key):
+    if "flag{N1k_n3v3r_g3ts_d4t3s_4nd_1s_l00k1ng_4_sum1_spec14l_LOL_th1s_1s_sUch_4_l0n6_fl4g_1_sUr3_h0PE_th3r3s_en0ugh_spaces!}".lower() == key.lower():
+        return True, "Woah, images are totally exploitable too!"
+    else:
+        return False, "Sorry, no dating for you!"
