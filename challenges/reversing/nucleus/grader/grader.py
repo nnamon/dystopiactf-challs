6ddf041af4b2513fb5169a33b38d8b32d7a9dbd3
glit3h <glit3h@iwanttoplayaga.me>
2015-07-09 01:17:32 +0800

    fixed major screwup on nucleus

--- challenges/reversing/nucleus/grader/grader.py
+++ challenges/reversing/nucleus/grader/grader.py
@@ -1,5 +1,5 @@
 def grade(arg, key):
-    if "flag{th15_15_4PP13_M4p5_b4d}".lower() == key.lower():
+    if "flag{pWd_Wh3n_d3bugg1ng_c0mmit5}".lower() == key.lower():
         return True, "Congrats! You made the world a better place."
     else:
         return False, "Your muffins smell like s**t and so do your ideas."
